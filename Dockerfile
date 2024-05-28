3cc826263679af044626e50583b8743c8b5784d1
Oleg Podlipalin <backspace.by@gmail.com>
2024-05-28 19:22:05 +0300

    clean up repo

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,11 @@
 FROM prefecthq/prefect:2-python3.10
 
-# Install/uninstall additional libraries
 RUN apt-get update && apt-get install -y curl
 RUN pip install prefect-docker
 RUN pip uninstall -y requests
 RUN pip install requests==2.31.0
 
-# Copy the entrypoint script
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 
-# Set the entrypoint
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-# Function to check if Prefect server is up
 wait_for_server() {
   while ! curl -sSf http://127.0.0.1:4200/api/health > /dev/null; do
     echo "Waiting for Prefect server to be available..."
@@ -9,16 +8,11 @@ wait_for_server() {
   done
 }
 
-# Start the Prefect server
 prefect server start &
 
-# Wait for the Prefect server to be ready
 wait_for_server
 
-# Create a working pool (this writes to the database)
 prefect work-pool create --type docker ${PREFECT_DOCKER_WORKPOOL} || true
-
-# Start the worker in the background
 prefect worker start --pool ${PREFECT_DOCKER_WORKPOOL} &
 
 wait
